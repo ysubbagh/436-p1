ff16795086a35959dc7e1bce7ab563b2b5bf7a3d
ysubbagh <ydns10@gmail.com>
2024-01-10 23:05:20 +0000

    wrap up for today

--- WebCrawl.java
+++ WebCrawl.java
@@ -1,4 +1,14 @@
+/* WebCrawl 
+* Author: Yasmine Subbagh
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebCrawl{
     public static void main(String[] args){
@@ -34,7 +44,31 @@ public class WebCrawl{
     public static void hop(String url, int n, Map<String, Integer> history){
         if(n <= 0){return;} //reached enf of number to crawl
 
+        URL link;
+        InputStream buff = null;
+        BufferedReader br;
+        String line;
 
+        try{
+            link = new URL(url);
+            buff = link.openStream();
+            br = new BufferedReader((new InputStreamReader((buff))));
+
+            while((line = br.readLine()) != null){
+                //get links n print
+            }
+        }catch (MalformedURLException mue){
+            mue.printStackTrace();;
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }finally{
+            try{
+                if(buff != null) {buff.close();}
+            }catch (IOException ioe){
+                ioe.printStackTrace();
+            }
+        }
+        n--;
     }
 
 }
