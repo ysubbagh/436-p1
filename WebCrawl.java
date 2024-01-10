a2d8cb4861100bc8f3dca09d7d7b2b6f5c51840f
ysubbagh <ydns10@gmail.com>
2024-01-10 22:05:29 +0000

    add error handligng

--- WebCrawl.java
+++ WebCrawl.java
@@ -1,37 +1,40 @@
 import java.util.*;
 
 public class WebCrawl{
-    //global variables
     public static void main(String[] args){
         String url = "";
         int n = -1;
+
+        //error handling for arguemtns
+        if(args.length > 2 || args.length < 2){
+            System.out.println("Incorrect arguments passed!");
+            System.exit(0);
+        }
+
         //parse the arguments
         for(int i = 0; i < 2; i++){
             if(i == 0){ //get URL
                 url = args[i];
             }else if(i == 1){ //get num
-                n = Integer.parseInt(args[i]);
-            }else{ //error handling
-                if(!args[i].equals("")){
-                    System.out.println("Too many arguemnts passed!");
+                try{ //error handling for non int
+                    n = Integer.parseInt(args[i]);
+                }catch(NumberFormatException e){
+                    System.out.println("Second argument not interger.");
                     System.exit(0);
                 }
             }
         }
         
+        //start crawling and setup history storage
         Map<String, Integer> history = new HashMap<String, Integer>();
         hop(url, n, history);
 
     }
 
     public static void hop(String url, int n, Map<String, Integer> history){
-        if(n <= 0){return;}
+        if(n <= 0){return;} //reached enf of number to crawl
 
-    }
 
-    public static Boolean print(String url){
-
-        return false; //base case
     }
 
 }
